37632173bb7da17b15503fb3a65c4b0c660a9855
rociornav <alum.rdelr@iesalixar.org>
2020-11-03 13:45:55 +0100

    Actividades actualizadas con nombre

--- Unidad3/U3T1_Date/Ejercicio2/js/u3t1ej2.js
+++ Unidad3/U3T1_Date/Ejercicio2/js/u3t1ej2.js
@@ -9,3 +9,21 @@ en domingo desde este año hasta el año 2100.
 Recuerda que los meses empiezan desde el número 0.
 */
 
+
+function cantDomingos() {
+    let res = new Date(document.getElementById("party").value); //recoge el valor del calendario
+    let cantSundays = 0; //cuenta los domingos que cae el dia del cumple del usuario
+
+    let day = res.getDate(); //día del mes
+    let month = res.getMonth(); //mes
+    let yearActual = res.getFullYear(); //año actual
+
+    
+    for(let i = yearActual; i <= "2100"; i++) { 
+        let fechasCumpleVenideras = new Date(i, month, day);
+        if(fechasCumpleVenideras.getDay() === 0) {
+            cantSundays++;
+        }
+    }
+    return alert("Solución: "+cantSundays+" domingos hasta el año 2100");
+  }
---
+++ curso Javascript Intermedio/Objetos/js/arrays.js
@@ -0,0 +1,6 @@
+/*
+suponga que tienes un array con 3 valores de colores tipo string, claro.
+
+Mediante bucles, ir accediendo a cada valor e indicar. Me gusta el color tal, y asi en cada posición.
+*/
+
