1874aa57038390dc99e51d6dc0e40bb9ac99342e
rdelr <alum.rdelr@iesalixar.org>
2020-12-08 20:01:09 +0100

    nuevos ejercicios eventos

---
+++ Unidad 4/Tarea8_Ejercicios adicionales de eventos/Ejercicio 1/ej1.js
@@ -0,0 +1,27 @@
+/*
+Enunciado:
+
+Realizar un script que cada vez que el usuario redimensione la ventana nos indique  el ancho y alto de la ventana del navegador.
+
+Nota personal: https://www.w3schools.com/js/js_window_screen.asp
+*/
+//sin redimensionar
+//document.getElementById("size").innerHTML="Screen Width: " + screen.width +" px | Screen Height: " + screen.height+" px";
+
+const height = document.querySelector("#height span");
+const width = document.querySelector("#width span");
+
+// Insert values on load of page 
+window.onload = function () {
+    height.innerHTML = window.innerHeight;
+    width.innerHTML = window.innerWidth;
+};
+
+// Change values when window is resized 
+window.onresize = function () {
+
+    // Setting the current height & width 
+    // to the elements 
+    height.innerHTML = window.innerHeight;
+    width.innerHTML = window.innerWidth;
+}; 
---
+++ Unidad 4/Tarea8_Ejercicios adicionales de eventos/Ejercicio 2/ej2.js
@@ -0,0 +1,14 @@
+/*
+Ejercicio 2
+
+Realizar un script que responda estos eventos:
+
+Al pulsar botón del ratón el color de fondo del cuadro de mensaje debe ser amarillo (#FFFF00).
+Cuando se pulsa una tecla, el color de fondo debe ser azul (#CCE6FF).
+Al volver a mover el ratón, el color de fondo vuelve a ser blanco.
+Modifica el ejemplo anterior para que cuando se pulse sobre el ratón se muestren las coordenadas
+respecto del navegador y la pantalla.
+Cuando se pulse una tecla se deberá mostrar tanto el carácter como su código ascii.
+*/
+
+
