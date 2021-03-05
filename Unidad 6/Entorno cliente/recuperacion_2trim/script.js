d444db347c718bebc60987d27d92b22a030e145c
Rocio del Real <alum.rdelr@iesalixar.org>
2021-03-05 21:25:46 +0100

    Examen recuperacion 2trim

--- Unidad 6/Entorno cliente/recuperacion_2trim/script.js
+++ Unidad 6/Entorno cliente/recuperacion_2trim/script.js
@@ -1,5 +1,4 @@
 // Rocio del Real
-let jsonFilm = [];
 let jsonFilmFiltered = [];//array peliculas con los campos del enunciado
 let tipoPeticion = "";
 /*****************Peticiones XHR***************************************************** */
@@ -11,9 +10,8 @@ function loadJsonXHR() {
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                jsonFilm = filtrado(JSON.parse(xhr.responseText));
-                // arrayFilter(jsonFilm);
-                console.log(jsonFilm);
+                arrayFilter(JSON.parse(xhr.responseText));
+                console.log(jsonFilmFiltered);
 
                 postInsertarFilms_XHR();
             } else {
@@ -33,46 +31,38 @@ pero esta vez sin los campos "id","url","people", "species", "locations" y "vehi
 deberemos enviarlo mediante POST y con el header Content-type "application/json" a la URL 
 "insert_films.php".
 */
-/*
+
 function arrayFilter(json) {
     // return json.filter((fila) => fila.ccaa != "Totales");
     //recorremos el array de peliculas
-    // let objetoFiltrado = {};
-    jsonFilm.map((filaPelicula) => {
-        let colArray = ["title", "description", "director", "producer", "release_date"];
+    json.map((filaPelicula) => {
+        let nuevaPelicula = {};
+        let colArray = ["id", "title", "description", "director", "producer", "release_date", "rt_score", "url"];
         colArray.map((col) => {
-            if (col == "title") {
-                jsonFilmFiltered.push("title: " + filaPelicula[col]);
-            } else if (col == "description") {
-                jsonFilmFiltered.push("description: " + filaPelicula[col]);
-            } else if (col == "director") {
-                jsonFilmFiltered.push("director: " + filaPelicula[col]);
-            } else if (col == "producer") {
-                jsonFilmFiltered.push("producer: " + filaPelicula[col]);
-            } else if (col == "release_date") {
-                jsonFilmFiltered.push("release_date: " + filaPelicula[col]);
-            }
-
+            nuevaPelicula[col] = filaPelicula[col];
         });
+        jsonFilmFiltered.push(nuevaPelicula);
     });
-    // console.log("json filtrado:");
-    console.log(jsonFilmFiltered);//comprobar
-}*/
 
-function filtrado(json) {
-    return json.filter((fila) => fila.title || fila.description || fila.director || fila.producer || fila.release_date);
+    //Otra forma:
+    // json.map((filaPelicula) => {
+    //     const nuevaPelicula = {
+    //         title: filaPelicula.title,
+    //         description: filaPelicula.description,
+    //         director: filaPelicula.director,
+    //         producer: filaPelicula.producer,
+    //         release_date: filaPelicula.release_date,
+    //     };
+    //     jsonFilmFiltered.push(nuevaPelicula);
+    // });
+
 }
-
-
-
 
 
 /*
 array con datos peliculas filtrados, deberemos enviarlo mediante POST y con el header Content-type 
 "application/json" a la URL "insert_films.php"
 */
-
-
 function postInsertarFilms_XHR() {
     //Hacemos la peticion con comunicacion asincrona
     var xhr = new XMLHttpRequest();
@@ -80,9 +70,9 @@ function postInsertarFilms_XHR() {
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                jsonFilm = JSON.parse(xhr.responseText);
+                jsonFilmFiltered = JSON.parse(xhr.responseText);
                 console.log("Prueba 2:");
-                console.log(jsonFilm);
+                console.log(jsonFilmFiltered);
                 console.log("Success: todo ha ido OK con XHR");
                 tipoPeticion = "XMLHttpRequest";
                 crearTablaPelis();
@@ -97,7 +87,7 @@ function postInsertarFilms_XHR() {
     };
 
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(jsonFilm)); //asi le envio el cuerpo del json contenida en el array
+    xhr.send(JSON.stringify(jsonFilmFiltered)); //asi le envio el cuerpo del json contenida en el array
 
 }
 
@@ -111,7 +101,7 @@ function loadJsonFetch() {
         .then(data => {
             // console.log("Data fetch: " + data); Ojo: al pintarla con un string al lado, sale esto: [object Object],[object Object],etc
             // console.log(data);//Nota: poniendo solo la variable sí podemos comprobarlo 
-            jsonFilm = filtrado(data);//eliminamos los campos que no queremos
+            arrayFilter(data);//eliminamos los campos que no queremos
             tipoPeticion = "Fetch";
             peticion_RellenarPHP_Fetch();//llamar la insercion de datos al php aqui.
         })
@@ -122,18 +112,16 @@ function loadJsonFetch() {
 function peticion_RellenarPHP_Fetch() {
     fetch("insert_films.php", {
         method: 'POST', // or 'PUT'
-        body: JSON.stringify(jsonFilm), // data can be `string` or {object}!
+        body: JSON.stringify(jsonFilmFiltered), // data can be `string` or {object}!
         headers: {
             'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         }
     }).then(res => {
         if (res.status == 200) return res.json();
     })
         .then(data => {
-            infoJson = data;
+            jsonFilmFiltered = data;
             tipoPeticion = "Fetch";
-            // selectDinamico();
             crearTablaPelis();
             console.log('Success: ', "todo ha ido OK con fetch");
         })
@@ -148,8 +136,8 @@ function loadPersonajesFetch() {
     fetch('https://ghibliapi.herokuapp.com/people')
         .then(response => { return response.json() }) //o .then((response) => response.json())
         .then(data => {
-            personajesArr = data;//eliminamos los campos que no queremos
-            console.log(personajesArr);//prueba
+            filtroPersonajes(data);//eliminamos los campos que no queremos
+            // console.log(personajesArr);//prueba
             tipoPeticion = "Fetch Personajes";
             rellenarPersonajes_Fetch();//llamar la insercion de datos al php aqui.
         })
@@ -174,13 +162,31 @@ function rellenarPersonajes_Fetch() {
         .then(data => {
             personajesArr = data;
             tipoPeticion = "Fetch personajes";
-            // selectDinamico();
-            // crearTablaPersonajes();
             console.log('Success: ', "todo ha ido OK con fetch personajes");
         })
         .catch(error => console.error('Error:', error))//comprobacion de errores
 }
 
+function filtroPersonajes(json) {
+
+    json.map((filaPersonaje) => {
+        // let objeto ={};
+        // let colArrayP=["id","name","gender","age","eye_color","hair_color","films"];
+        // colArrayP.map((col)=>{
+
+        // });
+        let objeto = {
+            "id": filaPersonaje.id,
+            "name": filaPersonaje.name,
+            "gender": filaPersonaje.gender,
+            "age": filaPersonaje.age,
+            "eye_color": filaPersonaje.eye_color,
+            "hair_color": filaPersonaje.hair_color,
+            "films": filaPersonaje.films
+        }
+        personajesArr.push(objeto);
+    });
+}
 /************************************************************************* */
 
 /*
@@ -190,7 +196,7 @@ botón con el título "Personajes".
 */
 
 function crearTablaPelis() {
-    const tablaDiv = document.getElementById("tablaPersonajes");
+    const tablaDiv = document.getElementById("tablaPelisDiv");
     tablaDiv.innerHTML = "";//para evitar acumulaciones de tablas.
 
     const tituloTipoPeticion = document.createElement("h2");
@@ -199,9 +205,9 @@ function crearTablaPelis() {
 
     //creamos tabla
     const tablaElem = document.createElement("table");
-    tablaElem.id = "tablaPersonajes";
+    tablaElem.id = "tablaPelis";
     //CREAMOS LAS CABECERAS DE LA TABLA y la fila de la cabecera 
-    const headers = ["Title", "Description", "Director", "Producer", "Release_date", "Boton Personaje"];
+    const headers = ["Title", "Description", "Director", "Producer", "Release_date", "Rt_score", "Boton Personaje"];
     //recoremos el array headers y creamos las th y las pintamos con su valor
     const crearFilaHeader = document.createElement("tr");
     headers.map((h) => {
@@ -214,9 +220,9 @@ function crearTablaPelis() {
 
     //CREAMOS LAS FILAS GENERALES ENCONTRADA EN EL JSON POR ID EXISTENTE
     //recorremos el array del json con las peliculas filtradas
-    jsonFilm.map((filaPeli) => {
+    jsonFilmFiltered.map((filaPeli) => {
         const filaGeneral = document.createElement("tr");
-        const colArray = ["title", "description", "director", "producer", "release_date", "botonP"];
+        const colArray = ["title", "description", "director", "producer", "release_date", "rt_score", "botonP"];
         //CREAMOS LAS COLUMNAS
         //recorremos el array de las columnas cuyos elementos se llaman igual que las claves o propiedades de cada objeto que compone el json.
         colArray.map((elemento) => {
@@ -227,7 +233,7 @@ function crearTablaPelis() {
                 const botonElem = document.createElement("input");
                 botonElem.type = "button";
                 botonElem.value = "Personajes";
-                botonElem.setAttribute("onclick", "peticionPost()");
+                botonElem.setAttribute("onclick", "peticionPost_Personajes('" + filaPeli.id + "')");
                 columnElem.appendChild(botonElem);
             }
 
@@ -241,25 +247,23 @@ function crearTablaPelis() {
 
 /****************post por cada boton 'Personajes' de la tabla***************/
 //tenemos que pasarle un json con la id_film
-function filterIdFilm() {
-    let idFiltered = jsonFilm.filter((elem) => { return elem.id });
-    const objeto = {
-        "id_film": idFiltered
-    }
-}
+let listaNombresPersonajes = [];
 function peticionPost_Personajes(id) {
+    let jsonIdFilm = {
+        "id_film": id
+    };
     //Hacemos la peticion con comunicacion asincrona
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "people_by_film_id.php", true); //la url del php por parametro
     xhr.onload = function (e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                jsonFilm = JSON.parse(xhr.responseText);
-                console.log("Prueba 2:");
-                console.log(jsonFilm);
+                listaNombresPersonajes = JSON.parse(xhr.responseText);
+                console.log("Prueba lista names:");
+                console.log(listaNombresPersonajes);
                 console.log("Success: todo ha ido OK con XHR");
                 tipoPeticion = "XMLHttpRequest";
-                crearTablaPelis();
+                crearTablaPersonajes();
 
             } else {
                 console.error(xhr.statusText);
@@ -271,8 +275,47 @@ function peticionPost_Personajes(id) {
     };
 
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(jsonFilm)); //asi le envio el cuerpo del json
+    xhr.send(JSON.stringify(jsonIdFilm)); //asi le envio el cuerpo del json
 }
 
+function crearTablaPersonajes() {
+    const tablaDiv = document.getElementById("tablaPersonajesDiv");
+    tablaDiv.innerHTML = "";//para evitar acumulaciones de tablas.
+
+    // const tituloTipoPeticion = document.createElement("h2");
+    // tituloTipoPeticion.innerText = "Tabla creada con " + tipoPeticion;
+    // tablaDiv.appendChild(tituloTipoPeticion);
+
+    //creamos tabla
+    const tablaElem = document.createElement("table");
+    tablaElem.id = "tablaPersonajes";
+    //CREAMOS LAS CABECERAS DE LA TABLA y la fila de la cabecera 
+    const headers = ["Name", "Gender", "Age", "Eye_color", "Hair_color"];
+    //recoremos el array headers y creamos las th y las pintamos con su valor
+    const crearFilaHeader = document.createElement("tr");
+    headers.map((h) => {
+        const crearColHeader = document.createElement("th");
+        crearColHeader.innerText = h;
+        crearFilaHeader.appendChild(crearColHeader);//lo colocamos en la fila headers
+    });
+    tablaElem.appendChild(crearFilaHeader);//colocamos dicha fila en la tabla
+    tablaDiv.appendChild(tablaElem); //coloco la tabla en el dom
+
+    //CREAMOS LAS FILAS GENERALES ENCONTRADA EN EL JSON POR ID EXISTENTE
+    //recorremos el array del json con las peliculas filtradas
+    listaNombresPersonajes.map((filaPersonaje) => {
+        const filaGeneral = document.createElement("tr");
+        const colArray = ["name", "gender", "age", "eye_color", "hair_color"];
+        //CREAMOS LAS COLUMNAS
+        //recorremos el array de las columnas cuyos elementos se llaman igual que las claves o propiedades de cada objeto que compone el json.
+        colArray.map((elemento) => {
+            const columnElem = document.createElement("td");
+            columnElem.innerText = filaPersonaje[elemento];
+            filaGeneral.appendChild(columnElem); //colocamos cada columna en su fila.
+        });
+        tablaElem.appendChild(filaGeneral);//colocamos dichas filas en la tabla
+    });
+
+}
 
 
